1f9a4664243c0388c44df3af0aa5666ede8aeebc
Your Name <ktrequina.student@asiancollege.edu.ph>
2026-05-30 17:06:34 +0800

    eere

--- Suelto Porfolio/src/components/Navbar.jsx
+++ Suelto Porfolio/src/components/Navbar.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="nav-logo">cvbs.</div>
+     <div className="nav-logo jumping-letters">
+  <span>c</span>
+  <span>v</span>
+  <span>b</span>
+  <span>s</span>
+  <span>.</span>
+</div>
       <ul className="nav-links">
         <li><a href="#home" className="nav-item">Home</a></li>
         <li><a href="#about" className="nav-item">About</a></li>
