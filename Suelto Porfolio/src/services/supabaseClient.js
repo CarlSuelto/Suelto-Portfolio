b7f03828a0d8af73e02df20ed9e65f117ab6f2fc
Your Name <ktrequina.student@asiancollege.edu.ph>
2026-05-30 13:58:51 +0800

    git push

--- Suelto Porfolio/src/services/supabaseClient.js
+++ Suelto Porfolio/src/services/supabaseClient.js
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-io'; // standard native fetch client or '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+
